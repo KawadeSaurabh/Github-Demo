792d94d66e95293fc2e3784ccebfbe03b22d8798
KawadeSaurabh <kawade.saurabh1996@gmail.com>
2024-01-09 09:24:35 +0530

    Adding Project

---
+++ src/main/java/com/sk/Github1/GithubDemo11Application.java
@@ -0,0 +1,13 @@
+package com.sk.Github1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDemo11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/Github1/GithubDemo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.Github1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDemo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
